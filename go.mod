3d5fad8274e25e285e3ea20939d10a22e4291918
Shawn Sherwood <ssherwood@vmware.com>
2022-02-01 12:41:34 -0500

    Initial demo code added

---
+++ go.mod
@@ -0,0 +1,15 @@
+module yb-gocql
+
+go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/yugabyte/gocql v0.0.0-20211022122817-e03782bd564c
+)
+
+require (
+	github.com/gocql/gocql v0.0.0-20211222173705-d73e6b1002a7 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/yugabyte/gocql"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	log.Println("Starting...")
+
+	session := getYCQLSession()
+	defer session.Close()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/init", func(w http.ResponseWriter, r *http.Request) { InitData(session, w, r) }).Methods("GET")
+	router.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) { Search(session, w, r) }).Methods("GET")
+
+	// serve the app
+	fmt.Println("Server at 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getYCQLSession() *gocql.Session {
+	cluster := gocql.NewCluster("127.0.0.1") //, "127.0.0.2", "127.0.0.3")
+	cluster.Port = 9042
+	cluster.ProtoVersion = 4
+	//cluster.DisableSkipMetadata = false // TODO why did we change the defaults?
+	// TODO is this CQLVersion correct?
+	cluster.CQLVersion = "3.4.2"               // CQL version (default: 3.0.0)
+	cluster.ConnectTimeout = 12 * time.Second  // initial connection timeout, used during initial dial to server (default: 600ms)
+	cluster.SocketKeepalive = 10 * time.Second // The keepalive period to use, enabled if > 0 (default: 0)
+	cluster.Timeout = 12 * time.Second         // connection timeout (default: 600ms)
+	cluster.Consistency = gocql.Quorum         // default consistency level (default: Quorum); "One" enables follow reads
+	//cluster.Keyspace = "default" <- causes a segv on tserver
+
+	// compression algorithm (default: nil)
+	//cluster.Compressor = gocql.SnappyCompressor{}
+	// ^ causes errors in the log
+
+	// This uses the partition key "awareness" to route requests to the correct tserver host
+	// It falls back to a datacenter aware round-robin approach...
+	// check pool->hostConnPools
+	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.DCAwareRoundRobinPolicy("cloud1.datacenter1.rack1"))
+	//cluster.PoolConfig.HostSelectionPolicy = gocql.DCAwareRoundRobinPolicy("fooo")
+
+	// Retry policy: https://pkg.go.dev/github.com/gocql/gocql#hdr-Retries_and_speculative_execution
+	// BE CAREFUL WITH THIS...
+	// Retry on reads w/ an aggressive cluster.Timeout can lead to a request storm during leader elections...
+	//cluster.RetryPolicy = &gocql.ExponentialBackoffRetryPolicy{
+	//	NumRetries: 5,
+	//	Min:        50 * time.Millisecond, // not sure how this works, it's not really documented?
+	//	Max:        2 * time.Second,
+	//}
+
+	// Default reconnection policy to use for reconnecting before trying to mark host as down (default: see below)
+	//cluster.ReconnectionPolicy
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+
+	// TODO, what is this supposed to show?
+	session.SetTrace(gocql.NewTraceWriter(session, os.Stdout))
+	return session
+}
+
+func InitData(session *gocql.Session, _response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	queryParams := request.URL.Query()
+
+	log.Println("Called InitData")
+	log.Println("Vars:", vars)
+	log.Println("Params:", queryParams)
+
+	dataCount := 1_000 // default to 1000 rows
+	if v, found := queryParams["count"]; found {
+		dataCount, _ = strconv.Atoi(v[0])
+	}
+
+	// set up the keyspace
+	log.Println("Initializing 'demo' KEYSPACE...")
+	if err := session.Query("CREATE KEYSPACE IF NOT EXISTS demo").Exec(); err != nil {
+		log.Fatal(err)
+	}
+
+	// set up the table
+	log.Println("Initializing 'demo' TABLE...")
+	if err := session.Query(`CREATE TABLE
+                                 IF NOT EXISTS demo.demo(
+									primaryId text,
+									secondaryId text,
+									clusterCol1 text,
+									clusterCol2 text,
+									dataCol1 int,
+									dataCol2 boolean,
+									dataCol3 timestamp,
+									PRIMARY KEY((primaryId, secondaryId), clusterCol1, clusterCol2)
+                                 )`).Exec(); err != nil {
+		log.Fatal("create table if not exists:", err)
+	}
+
+	// load initial data
+	log.Println("Initializing dataset with", dataCount, " x 2 rows...")
+
+	for i := 0; i < dataCount; i++ {
+		for j := 0; j < 2; j++ {
+			if err := session.Query(`INSERT INTO demo.demo(
+										primaryId, secondaryId,
+										clusterCol1, clusterCol2,
+										dataCol1, dataCol2,	dataCol3)
+									       VALUES (?, ?, ?, ?, ?, ?, ?)`,
+				"P1", fmt.Sprintf("%013d", i), fmt.Sprintf("FOO%d", j), "BAR", rand.Intn(100), rand.Intn(2) == 1, time.Now()).Exec(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	log.Println("Initialization done!")
+}
+
+func Search(session *gocql.Session, response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	queryParams := request.URL.Query()
+
+	log.Println("Called Search")
+	log.Println("Vars:", vars)
+	log.Println("Params:", queryParams)
+
+	query := `SELECT secondaryId, clusterCol1, clusterCol2, dataCol1, dataCol2, dataCol3
+                FROM demo.demo
+               WHERE primaryId = ? AND secondaryId IN(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+	params := []interface{}{"P1"}
+	// TODO make this dynamic on queryParams
+	for i := 0; i < 10; i++ {
+		params = append(params, fmt.Sprintf("%013d", rand.Intn(1000)))
+	}
+
+	iter := session.Query(query, params...).Iter()
+
+	var resultData []ResultData
+	var secondaryId string
+	var clusterCol1 string
+	var clusterCol2 string
+	var dataCol1 int
+	var dataCol2 bool
+	var dataCol3 time.Time
+
+	for iter.Scan(&secondaryId, &clusterCol1, &clusterCol2, &dataCol1, &dataCol2, &dataCol3) {
+		resultData = append(resultData, ResultData{
+			SecondaryId: secondaryId,
+			ClusterCol1: clusterCol1,
+			ClusterCol2: clusterCol2,
+			DataCol1:    dataCol1,
+		})
+	}
+
+	if err := iter.Close(); err != nil {
+		log.Println(err)
+		json.NewEncoder(response).Encode(JsonResponse{Type: "error", Message: err.Error()})
+	} else {
+		json.NewEncoder(response).Encode(JsonResponse{Type: "success", Data: resultData})
+	}
+}
+
+type JsonResponse struct {
+	Type    string       `json:"type"`
+	Data    []ResultData `json:"data"`
+	Message string       `json:"message"`
+}
+
+type ResultData struct {
+	PrimaryId   string    `json:"primary_id"`
+	SecondaryId string    `json:"secondary_id"`
+	ClusterCol1 string    `json:"cluster_col_1"`
+	ClusterCol2 string    `json:"cluster_col_2"`
+	DataCol1    int       `json:"data_col_1"`
+	DataCol2    bool      `json:"data_col_2"`
+	DataCol3    time.Time `json:"data_col_3"`
+}
